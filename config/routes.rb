0293af9ebf2646267112844a3f537f7e1c5055f0
Derrick Lannaman <derricklannaman@gmail.com>
2015-01-24 23:25:44 -0500

    add more link test and links

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'about', to: 'home#about'
-
+  get 'contact', to: 'home#contact'
+  get 'learn', to: 'home#learn'
 end
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,18 +3,28 @@ require "rails_helper"
 describe 'When visiting the home page' do
 
   it "has the title 'SusuBank'" do
-    visit root_path
+    visit "/"
     expect(page).to have_content('SusuBank')
   end
 
   it "has an 'about page' link" do
-    visit root_path
+    visit "/"
     expect(page).to have_link("About", about_path)
   end
 
+  it "has a 'contact page' link" do
+    visit "/"
+    expect(page).to have_link("Contact", contact_path)
+  end
 
-  it "has a 'contact page' link"
-  it "has a 'learn more' page link"
+  it "has a 'learn more' page link" do
+    visit "/"
+    expect(page).to have_link("Learn More", learn_path)
+  end
+
+
+
+
   it "allows the 'About' link to visit About page"
   it "allows the 'Contact' link to visit About page"
   it "allows the 'Learn More' page to visit Learn More"
