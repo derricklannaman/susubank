3ed773d9cc439588ed2fdd06b8f4ddd0e69d5c62
Derrick Lannaman <derricklannaman@gmail.com>
2015-01-30 23:01:19 -0500

    change fields to text-fields, create dashboard for users

--- app/models/susu_user.rb
+++ app/models/susu_user.rb
@@ -1,6 +1,17 @@
 class SusuUser < ActiveRecord::Base
+  after_save :create_dashboard
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+
+private
+
+  def create_dashboard
+    Dashboard.create(susu_user_id: self.id)
+  end
+
 end
